da6185adeea17a3ebe7e7a15dfa9a6e84d0384af
n-katou <102382177+n-katou@users.noreply.github.com>
2023-01-24 16:06:41 +0900

    機能完成

--- src/main/java/com/naoto/portfolio/app/controller/TodoController.java
+++ src/main/java/com/naoto/portfolio/app/controller/TodoController.java
@@ -1,11 +1,14 @@
 package com.naoto.portfolio.app.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.naoto.portfolio.app.service.TodoService;
 import com.naoto.portfolio.domain.todos.model.Todo;
@@ -17,10 +20,44 @@ public class TodoController {
     TodoService todoService;
    
     @GetMapping("/todos")
-    public String index(Model model) {
-        List<Todo> allTodo = todoService.searchAll();
-        model.addAttribute("allTodo", allTodo);
-        return "/todos/index";
+    public String index(@ModelAttribute Todo todo, Model model) {
+        model.addAttribute("allTodo", todoService.searchAll());
+        return "todos/index";
     }
+
+    @PostMapping("/create")
+    public String createTodo(@Validated @ModelAttribute Todo todo, BindingResult bindingResult,Model model ) {
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("allTodo", todoService.searchAll());
+            return "todos/index";
+        }
+        todoService.addTodo(todo);
+        return "redirect:/todos";
+    }
+
+    @PostMapping("/done")
+    public String doneTodo(@RequestParam(name = "id") Integer todoId) {
+        Todo updaTodo = todoService.findById(todoId);
+        updaTodo.setDone(true);
+        todoService.addTodo(updaTodo);
+        return "redirect:/todos";
+    }
+
+    @PostMapping("/notdone")
+    public String notdoneTodo(@RequestParam(name = "id") Integer todoId) {
+        Todo updaTodo = todoService.findById(todoId);
+        updaTodo.setDone(false);
+        todoService.addTodo(updaTodo);
+        return "redirect:/todos";
+    }
+
+
+    @PostMapping("/deleteAll")
+    public String deleteAll() {
+        todoService.deleteAllTodo();
+        return "redirect:/todos";
+    }
+
 
 }
--- src/main/java/com/naoto/portfolio/domain/todos/model/Todo.java
+++ src/main/java/com/naoto/portfolio/domain/todos/model/Todo.java
@@ -1,10 +1,16 @@
 package com.naoto.portfolio.domain.todos.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
@@ -17,9 +23,16 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-  
+    
+    @NotNull
+    @NotBlank
     private String content;
   
+
     private boolean done;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate time;
     
 }
